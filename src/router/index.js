e0a5e9214fa3edaeb8f4f49e4dc84eb9512afa48
unknown <harshkapoork>
2025-08-31 03:12:42 +0530

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import Home from '@/Pages/Home.vue'
+
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [{ path: '/', component: Home }],
+})
+
+export default router
